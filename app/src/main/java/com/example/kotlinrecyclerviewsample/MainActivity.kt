708b4db9293cc1fa43b34ee01a01d777c121a113
ANIL YILMAZ <uyelikanil@gmail.com>
2020-12-28 12:03:37 +0300

    Recyclerview developed

--- app/src/main/java/com/example/kotlinrecyclerviewsample/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrecyclerviewsample/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.kotlinrecyclerviewsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinrecyclerviewsample.adapter.ItemAdapter
+import com.example.kotlinrecyclerviewsample.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val dataset = Datasource().loadAffirmations()
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this,dataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
